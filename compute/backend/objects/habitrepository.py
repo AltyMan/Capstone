47b764637340ecdd98cf6a33b704d9f0920e33a7
Supercobra27 <silverbergryan@gmail.com>
2026-01-13 21:19:24 -0500

    added add_rule and fixes

--- compute/backend/objects/habitrepository.py
+++ compute/backend/objects/habitrepository.py
@@ -2,6 +2,7 @@ from db.sqlite import get_connection
 from datetime import datetime
 from utils.time import _timestamp
 from objects.habit import Habit
+from objects.rule import Rule
 from typing import Optional
 from pandas import DataFrame
 
@@ -12,8 +13,8 @@ class HabitRepository:
     def add(self, habit_name: str, is_device: bool = False):
         with get_connection() as conn:
             conn.execute(
-                "INSERT INTO habits (user_id, job_id, name, is_device) VALUES (?, ?, ?, ?)",
-                (self.user_id, "Not Implemented", habit_name, int(is_device))
+                "INSERT INTO habits (user_id, name, is_device) VALUES (?, ?, ?)",
+                (self.user_id, habit_name, int(is_device))
             )
 
     def get_all(self) -> list[Habit]:
@@ -26,7 +27,7 @@ class HabitRepository:
         habits: list[Habit] = [] 
         for r in rows:
             h = Habit(
-                name = r['name'],
+                habit_name = r['name'],
                 is_device=bool(r['is_device']),
                 assoc_mqtt_topic=r['mqtt_topic']
             )
@@ -62,3 +63,28 @@ class HabitRepository:
             
             df = DataFrame(rows, columns=rows[0].keys())
             return df
+        
+    def add_rule(self, habit_id: int, rule: Rule):
+        with get_connection() as conn:
+            conn.execute(
+                """
+                INSERT INTO habit_rules (user_id, job_id, habit_id, day, hour, minute, count)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                """,
+                (self.user_id, f"{self.user_id}_job_test", habit_id, rule.day, rule.hour, rule.minute, 1)
+            )
+        
+    def get_rules(self):
+        with get_connection() as conn:
+            conn.execute(
+                """
+                SELECT * FROM habit_rules WHERE user_id = ?
+                """,
+                (self.user_id,)
+            )
+        
+    def upsert_rule(self):
+        return
+    
+    def delete_rule(self):
+        return
