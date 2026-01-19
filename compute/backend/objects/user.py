42600f437b73637dde93ca9314e1e29c09258cbc
Supercobra27 <silverbergryan@gmail.com>
2026-01-19 16:34:16 -0500

    openings for user

--- compute/backend/objects/user.py
+++ compute/backend/objects/user.py
@@ -22,21 +22,25 @@ class User:
     def __init__(self, user_id: int):
         self.id = user_id
         self.repo = HabitRepository(user_id)
-        
-    def get_habits(self) -> list[Habit]:
-        return self.repo.get_all()
     
     def add_habit(self, habit_name: str, is_device: bool = False):
         self.repo.add(habit_name, is_device)
+    
+    def get_habits(self) -> list[Habit]:
+        return self.repo.get_all()
+    
+    def update_habit(self, field: str, value: str, habit_name: str):
+        self.repo.update_habit(field, value, habit_name)
+    
+    def delete_habit(self, habit_name: str):
+        self.repo.delete_habit(habit_name)
             
     def log_habit(self, habit_name: str, state: str = "NaV", self_reported: bool = True):
         self.repo.log(habit_name, state, self_reported)
     
     def get_habit_logs(self, habit_name: Optional[str] = None):
         return self.repo.get_logs(habit_name)
-    
-    def update_habit(self, habit_name: Optional[str] = None):
-        pass
+
     
     def add_rule(self, habit_id: int, rule: Rule):
         self.repo.add_rule(habit_id, rule)
