a9cfdb4e7efba894d39a93a1d23fceca151deef2
Supercobra27 <silverbergryan@gmail.com>
2026-01-19 16:33:58 -0500

    commented out FKs so I can test

--- compute/backend/db/sqlite.py
+++ compute/backend/db/sqlite.py
@@ -9,6 +9,7 @@ def get_connection() -> sqlite3.Connection:
     DB_PATH.parent.mkdir(exist_ok=True)
     conn: sqlite3.Connection = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
+    # conn.execute("PRAGMA foreign_keys = ON;") 
     return conn
 
 def reset_habit_tables(conn: sqlite3.Connection) -> None:
