1c6fff9f821587ad522579e13bc4614cfef5cd1c
Supercobra27 <silverbergryan@gmail.com>
2026-02-02 15:16:38 -0500

    allow for single image capture

--- compute/cv/cv.py
+++ compute/cv/cv.py
@@ -2,20 +2,25 @@ import cv2 as cv
 
 import cv2
 from ultralytics import YOLO
-
+import numpy as np
 # Load pretrained YOLOv8 model
 model = YOLO("C:\\Users\\Owner\\Documents\\Dev\\Capstone\\compute\\cv\\runs2\\content\\runs\\detect\\train\\weights\\best.pt")  # nano model (fast)
-
+model.predict(source=np.zeros((640, 640, 3), dtype=np.uint8), verbose=False)
 # Open webcam (0 = default camera)
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     raise RuntimeError("Could not open camera")
 
-while True:
+# warm up camera
+for _ in range(10):   # 5–30 is typical
+    cap.read()
+
+
+def capture_cv_image():
     ret, frame = cap.read()
     if not ret:
-        break
+        return
 
     # Run object detection
     results = model(frame, stream=True)
@@ -38,10 +43,17 @@ while True:
                 (0, 255, 0),
                 2,
             )
+    return frame
 
-    cv2.imshow("Object Detection", frame)
+frame = capture_cv_image()
+cv2.imshow("Object Detection", frame)
 
+while True:
     if cv2.waitKey(1) & 0xFF == ord("q"):
+        frame = capture_cv_image()
+        cv2.imshow("Object Detection", frame)
+        
+    if cv2.waitKey(1) & 0xFF == ord("e"):
         break
 
 cap.release()
