69d1d393fda1e64ce2abaed7ddeb68ed24966a8d
Amit Altman (21AA255) <amit.altman@queensu.ca>
2026-03-06 12:47:18 -0500

    Amit App Changes

--- compute/mobile/habittrackerapp/lib/habo_client.dart
+++ compute/mobile/habittrackerapp/lib/habo_client.dart
@@ -5,11 +5,11 @@ import '../../rule.dart';
 
 
 Future<void> main() async {
-  final habit_repo = HttpHabitRepository(1);
-  final rule_repo = HttpRuleRepository(1);
+  final habitRepo = HttpHabitRepository(1);
+  final ruleRepo = HttpRuleRepository(1);
 
   try {
-    await habit_repo.create(Habit(id: 1, name: "Tester", isDevice: true));
+    await habitRepo.create(Habit(id: 1, name: "Tester", isDevice: true));
     print('Habit created successfully');
   } catch (e) {
     print('Error: $e');
