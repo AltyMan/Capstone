6f87415b44778e47390d695b25136708c1171fa7
Supercobra27 <silverbergryan@gmail.com>
2026-03-10 15:21:40 -0400

    push

--- compute/backend/test/show.sql
+++ compute/backend/test/show.sql
@@ -1,3 +1,9 @@
+-- INSERT INTO habits (user_id, mqtt_topic, habit_name, is_device, assoc_dev_id) VALUES (1, "idk/idk", "plug3", True, "hs105");
+
+DELETE FROM habit_logs WHERE habit_name = 'plug1';
+DELETE FROM habit_logs WHERE habit_name = 'plug2';
+DELETE FROM habit_logs WHERE habit_name = 'plug3';
+
 SELECT * FROM habit_rules WHERE user_id = 1;
 SELECT * FROM habits WHERE user_id = 1;
 SELECT * from habit_logs WHERE user_id = 1;
